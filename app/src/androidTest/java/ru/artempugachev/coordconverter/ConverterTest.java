0a1665b32a7390106c6101026521baf6f1fbbc77
artem-p <artempugachev1@gmail.com>
2016-01-31 23:09:01 +0300

    Заглушка для теста

---
+++ app/src/androidTest/java/ru/artempugachev/coordconverter/ConverterTest.java
@@ -0,0 +1,14 @@
+package ru.artempugachev.coordconverter;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+import junit.framework.TestCase;
+
+/**
+ * Created by artem on 31.01.16.
+ */
+public class ConverterTest extends TestCase {
+    public void testTrue() {
+        assertTrue(true);
+    }
+}
