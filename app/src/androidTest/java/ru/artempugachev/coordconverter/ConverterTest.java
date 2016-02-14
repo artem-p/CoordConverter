df114c0be3e22d1b6daad1e601c72606296cfd75
artem-p <artempugachev1@gmail.com>
2016-02-15 00:42:08 +0300

    Еще тесты

--- app/src/androidTest/java/ru/artempugachev/coordconverter/ConverterTest.java
+++ app/src/androidTest/java/ru/artempugachev/coordconverter/ConverterTest.java
@@ -24,6 +24,7 @@ public class ConverterTest extends TestCase {
         checkDMStoDDD(-18, 38, 46.48, -18.646244444444445);
         checkDMStoDDD(82, 58, 7.50, 82.96875);
         checkDMStoDDD(67, 36, 14.36, 67.60398889);
+        checkDMStoDDD(63, 23, 29.48, 63.39152222);
     }
 
     private void checkDMStoDDD(int deg, int min, double sec, double degExpected) {
@@ -33,7 +34,9 @@ public class ConverterTest extends TestCase {
     }
 
     public void testConvertDDDtoDMS() {
-        checkDDDtoDMS(67.60398889, 67, 36, 14.36);
+        checkDDDtoDMS(-34.30714385628803, -34, 18, 25.717882636908);
+        checkDDDtoDMS(63.39178056, 63, 23, 30.410016);
+        checkDDDtoDMS(67.60398889, 67, 36, 14.360004);
         checkDDDtoDMS(55.8, 55, 48, 0.0);
         checkDDDtoDMS(30.0, 30, 0, 0.0);
         checkDDDtoDMS(60.9554, 60, 57, 19.44);
