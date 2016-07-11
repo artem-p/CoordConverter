c474c40db221a52dbaa77167f902b821aeb23f2a
artem-p <artempugachev1@gmail.com>
2016-07-11 18:33:33 +0300

    Заготовки вкладок

--- app/src/main/java/ru/artempugachev/concoord/MainActivity.java
+++ app/src/main/java/ru/artempugachev/concoord/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_no_tabs);
+        setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
